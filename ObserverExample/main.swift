35363b082ed27c2552bb6fad266e7e038b1ac75d
Andrew <andymuncey@users.noreply.github.com>
2025-12-17 10:32:20 +0000

    Add example

--- ObserverExample/main.swift
+++ ObserverExample/main.swift
@@ -7,5 +7,86 @@
 
 import Foundation
 
-print("Hello, World!")
+
+enum Result {
+    case first
+    case upperSecond
+    case lowerSecond
+    case third
+    case fail
+}
+
+
+protocol ResultsPublicationObserver : AnyObject { //must be an object (not a struct)
+    func update(notice: String)
+}
+
+//subject
+class RegistryServices {
+    
+    var resultsObservers = [ResultsPublicationObserver]()
+    
+    
+    func processResults() {
+        
+        print("Holding Awards assessment board")
+        print("Results being processed")
+        notifyObservers()
+    }
+    
+    func notifyObservers() {
+        for observer in resultsObservers {
+            observer.update(notice: "Results are now available for checking")
+        }
+    }
+    
+    func register(observer: ResultsPublicationObserver){
+        resultsObservers.append(observer)
+    }
+    
+    func unRegister(observer: ResultsPublicationObserver) {
+        resultsObservers.removeAll { registeredObserver in
+            observer === registeredObserver
+        }
+    }
+    
+    
+}
+
+
+
+class Student : ResultsPublicationObserver {
+    func update(notice: String) {
+        print("\(name): \"I'm going to check my results")
+    }
+    
+    init (name: String) {
+        self.name = name
+    }
+    
+    var name: String
+    
+    
+}
+
+
+
+let studentA = Student(name: "Andy")
+let studentB = Student(name: "Bethany")
+let studentC = Student(name: "Chritina")
+
+let registry = RegistryServices()
+
+print("First year")
+registry.register(observer: studentA)
+registry.register(observer: studentB)
+
+registry.processResults() //C is not notified
+
+registry.unRegister(observer: studentA)
+
+print("Second year")
+registry.processResults()
+
+
 
